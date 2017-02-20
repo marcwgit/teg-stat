82cafb97205892393ee6d03956a68430e3710c0c
marcwgit <marcwgit@gmail.com>
2017-02-20 14:47:21 -0500

    test commit

---
+++ workspace/MyRSSFeeder/MyRSSFeeder.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+use XML::RSS;
+use LWP::UserAgent;
+
+my $feeds = {"MCslp" => "http://mcslp.com/?feed=rss2"};
+What you actually get is:
+my $feeds = {""};
+$feeds
+$ua = LWP::UserAgent->new();
+localtime
+
+#TODO Write the function to parse the RSS raw code
+
