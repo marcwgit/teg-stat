2fa9b770896a50e400eeea33878358cd2565712a
marcwgit <marcwgit@gmail.com>
2018-03-26 19:05:17 -0400

    new

--- test.bat
+++ test.bat
@@ -1,1 +1,6 @@
+type C:\Users\marcwe\AppData\Local\Tegrity\RecordingManager\ApplicationData\Logs\Archive\recording-manager.00000.log > TegDwnld.log
 
+cd C:\Users\marcwe\AppData\Local\Tegrity\RecordingManager\ApplicationData\Logs\
+@echo off
+for /R %%f in (*.log) do type "%%f" >> c:\users\marcwe\output.txt
+cd C:\Users\marcwe\
