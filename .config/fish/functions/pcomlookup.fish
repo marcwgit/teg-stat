1ab2b495a679e7381f4637899694fa247249baa1
marcwgit <marcwgit@gmail.com>
2018-03-27 10:17:08 -0400

    Delete pcomlookup.fish

--- .config/fish/functions/pcomlookup.fish
+++
@@ -1,3 +0,0 @@
-function pcomlookup
-	nslookup m.pcom.edu;nslookup m.pcom.edu ns1.comcastbusiness.net;nslookup m.pcom.edu 8.8.8.8;nslookup www.pcom.edu;nslookup www.pcom.edu ns1.comcastbusiness.net;nslookup www.pcom.edu 8.8.8.8;nslookup web.pcom.edu;nslookup web.pcom.edu ns1.comcastbusiness.net;nslookup web.pcom.edu 8.8.8.8;nslookup dev.pcom.edu;nslookup dev.pcom.edu ns1.comcastbusiness.net;nslookup dev.pcom.edu 8.8.8.8;nslookup pcs.pcom.edu;nslookup pcs.pcom.edu ns1.comcastbusiness.net;nslookup pcs.pcom.edu 8.8.8.8;
-end
